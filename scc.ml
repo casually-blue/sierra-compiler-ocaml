4086b7077d6e4c211d216b013a8b35406f5bee2b
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-23 17:09:09 -0600

    Add oneof parser

--- scc.ml
+++ scc.ml
@@ -1,9 +1,12 @@
-type parser_error = ExpectationError of string
+type parser_error = 
+        | ExpectationError of string
+        | EndOfInputError
+        | ListError of parser_error list
 
-type parser_f = string -> (char * string, parser_error) result
+type 'parsed parser_f = string -> ('parsed * string, parser_error) result
 
 let get_char (s: string): (char * string, parser_error) result = (match (String.length s) with
-	| 0 -> Error (ExpectationError "character")
+	| 0 -> Error EndOfInputError
 	| _ -> Ok (String.get s 0, (String.sub s 1 ((String.length s) - 1))))
 
 let match_char f err = fun s -> (
@@ -22,16 +25,16 @@ let match_digit = match_char isdigit (ExpectationError "digit")
 
 let match_alpha = match_char isalpha (ExpectationError "alphabetic character")
 
-let either (a: parser_f) (b: parser_f) = fun s -> (
-	match (a s) with
-	| Ok (c, rest) -> Ok (c, rest)
-	| Error (ExpectationError e1) -> (
-		match (b s) with
-		| Ok (c, rest) -> Ok (c, rest)
-		| Error (ExpectationError e2) -> Error (ExpectationError (e1 ^ " or " ^ e2))
-	)
-)
+let rec one_of (ps: ('parsed parser_f) list) (s: string) =
+        match ps with
+                | [] -> Error (ListError [])
+                | p :: prest -> (
+                        match (p s) with 
+                        | Error e -> (match (one_of prest s) with
+                                | Error ListError errs -> Error (ListError (e :: errs))
+                                | good -> good
+                        )
+                        | good -> good
+                )
 
-let seq (ps : parser_f list) = List.length ps
-
-let match_alnum = (either match_digit match_alpha)
+let match_alnum = one_of (match_digit :: match_alpha :: [])
