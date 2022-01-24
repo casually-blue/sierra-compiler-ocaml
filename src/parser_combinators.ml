964860afc1fcff4c990db5870be6fc16eb72f359
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-24 17:27:05 -0600

    Add right recursive chain

--- src/parser_combinators.ml
+++ src/parser_combinators.ml
@@ -6,15 +6,15 @@ type parser_error =
 type 'parsed parser_f = string -> ('parsed * string, parser_error) result
 
 let get_char (s: string): (char * string, parser_error) result = (match (String.length s) with
-	| 0 -> Error EndOfInputError
-	| _ -> Ok (String.get s 0, (String.sub s 1 ((String.length s) - 1))))
+        | 0 -> Error EndOfInputError
+        | _ -> Ok (String.get s 0, (String.sub s 1 ((String.length s) - 1))))
 
 let match_char (f: char -> bool) (err: parser_error) (s: string) = match (get_char s) with
-	| Ok (c, rest) -> (
-		match (f c) with
-		| true -> Ok (c, rest)
-		| false -> Error err)
-	| Error e -> Error e
+        | Ok (c, rest) -> (
+                match (f c) with
+                | true -> Ok (c, rest)
+                | false -> Error err)
+        | Error e -> Error e
 
 let charp c = match_char (fun ch -> c == ch) (ExpectationError (String.make 1 c))
 
@@ -28,24 +28,19 @@ let match_alpha = match_char isalpha (ExpectationError "alphabetic character")
 let rec one_of (ps: ('parsed parser_f) list) (s: string) =
         match ps with
                 | [] -> Error (ListError [])
-                | p :: prest -> (
-                        match (p s) with 
+                | p :: prest -> (match (p s) with 
                         | Error e -> (match (one_of prest s) with
                                 | Error ListError errs -> Error (ListError (e :: errs))
-                                | good -> good
-                        )
-                        | good -> good
-                )
+                                | good -> good)
+                        | good -> good)
 
 let match_alnum = one_of (match_digit :: match_alpha :: [])
 
 let rec many1 (p: 'parsed parser_f) (s: string) =
         match (p s) with
-                | Ok (result, rest) -> (
-                        match (many1 p rest) with
+                | Ok (result, rest) -> (match (many1 p rest) with
                                 | Ok (results, rest) -> Ok (result :: results, rest)
-                                | Error _ -> Ok (result :: [], rest)
-                )
+                                | Error _ -> Ok (result :: [], rest))
                 | Error error -> Error error
 
 let many (p: 'parsed parser_f) (s: string) =
@@ -78,8 +73,7 @@ let (<+>) p1 p2 s = match (p1 s) with
 let (<|>) p1 p2 s = match (p1 s) with
         | Error e1 -> (match (p2 s) with
                 | Error e2 -> Error (ListError [e1; e2])
-                | good -> good
-        )
+                | good -> good)
         | good -> good
 
 let remove_whitespace p = parser_map (whitespace <+> p) (fun (_, r) -> r)
@@ -109,13 +103,10 @@ and chain_rest p op a s = match (op s) with
                         | Error e -> Error e)
         | Error _ -> Ok (a, s)
 
-
-
-
-
-
-
-
-
-
-
+let rec chainr1 (p: 'a parser_f) (op: ('a -> 'a -> 'a) parser_f) (s: string) = match (p s) with
+        | Ok (left, rest) -> (match (op rest) with
+                | Ok (oper, rest) -> (match (chainr1 p op rest) with
+                        | Ok (right, rest) -> Ok (oper left right, rest)
+                        | Error e -> Error e)
+                | Error _ -> Ok (left, rest))
+        | Error e -> Error e
