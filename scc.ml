8ea4ec358605413a148955292f269ad63b9eb3e8
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-26 13:05:17 -0600

    Add function parsing

--- scc.ml
+++ scc.ml
@@ -11,6 +11,8 @@ type expr =
         | Number of int
         | Binary of op * expr * expr
 
+
+
 let binary op left right = Binary (op, left, right)
 let number n = Number n
 
@@ -18,8 +20,39 @@ type stmt =
         | Expression of expr
         | Binding of string * expr
 
+type block = Block of (stmt list)
+let block stmts = Block stmts
+
+type func = Function of string * block
+let func name block  = Function (name, block)
+
 let stmt_expr exp = Expression exp
 let binding s exp = Binding (s,exp)
+
+(* recursively evaluate expressions *)
+let rec eval (expression: expr): int = match expression with
+        (* just give the value *)
+        | Number a -> a
+        (* get a function to apply to the left and right values *)
+        | Binary (op,left,right) -> (match op with
+                | Plus -> (+)
+                | Minus -> (-)
+                | Times -> fun a b -> (a * b)
+                | Divide -> (/)) 
+        (* recurse *)
+        (eval left) (eval right)
+
+let stmt_to_string stmt = match stmt with
+        | Expression e -> string_of_int (eval e)
+        | Binding (name,e) -> name ^ " = " ^ (string_of_int (eval e))
+
+let block_to_string (Block stmts) = List.fold_left
+                                (fun l r -> l ^ "\n\t" ^ (stmt_to_string r))
+                                ""
+                                stmts
+
+let func_to_string (Function (name, block)) = "function " ^ name ^ "{{ " ^ (block_to_string block) ^ "\n}}\n "
+
 
 (* parse number *)
 let number = pmap_ok integer (ok_construct number)
@@ -47,19 +80,9 @@ let stmt = pmap_ok
         ((binding <|> bare_expression) <-+> (charp ';'))
         (fun (exp, _) rest -> Ok (exp, rest))
 
-
-(* recursively evaluate expressions *)
-let rec eval (expression: expr): int = match expression with
-        (* just give the value *)
-        | Number a -> a
-        (* get a function to apply to the left and right values *)
-        | Binary (op,left,right) -> (match op with
-                | Plus -> (+)
-                | Minus -> (-)
-                | Times -> fun a b -> (a * b)
-                | Divide -> (/)) 
-        (* recurse *)
-        (eval left) (eval right)
+let funcp = pmap_ok
+        ((keyword "function") <-+> identifier <-+> (charp '(') <-+> (charp ')') <-+> (charp '{') <-+> (many stmt) <-+> (charp '}'))
+        (fun ((((((_, name), _), _), _), stmts), _) rest -> ok (func name (block stmts)) rest)
 
 
 (* REPL *)
@@ -74,14 +97,10 @@ let () = let exiting = ref false in while (not !exiting) do
         match line with
         | Some line -> (
                 (* Parse the line *)
-                match ((remove_whitespace (stmt <-+> eof)) line) with
+                match ((remove_whitespace (funcp <-+> eof)) line) with
                 (* Eval, throwing away the rest of input and the eof which got parsed *)
-                | Ok ((s, ()), _) -> (match s with
-                        | Binding (name, exp) ->
-                                        print_string ("Result: " ^ name ^ " = ") ; print_int (eval exp)
-                        | Expression exp -> print_string "Result: " ; print_int (eval exp); print_endline ""
-                        )
+                | Ok ((f, ()), _) -> print_string (func_to_string f)                 
                 (* if there is an error just print it out *)
-                        | Error (err, input) -> print_endline ("Error parse failed:" ^ (stringify_parser_error err) ^ " (" ^ input ^ ")");)
+                | Error (err, input) -> print_endline ("Error parse failed:" ^ (stringify_parser_error err) ^ " (" ^ input ^ ")");)
         | None -> exiting := true
 done
