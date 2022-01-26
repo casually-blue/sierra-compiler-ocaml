fb2f4a717ec9bf789058d5cbe8d1eb8ed46d6d62
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-26 16:28:15 -0600

    Add comments

--- src/parser.ml
+++ src/parser.ml
@@ -19,16 +19,20 @@ let binary_op term op ctor = chainl1 (remove_whitespace term) (pmap_ok (remove_w
 let term = binary_op number term_oper binary
 let expr = binary_op term expr_oper binary
 
+(* variable binding of the form "let x = expr" *)
 let binding = pmap_ok
                 ((keyword "let") <-+> identifier <-+> (charp '=') <-+> expr)
                 (fun (((_, name),_), exp) rest -> Ok (binding name exp, rest))
 
+(* just an expression *)
 let bare_expression = pmap_ok expr (ok_construct stmt_expr)
 
+(* parse either a binding or an expression followed by a semicolon *)
 let stmt = pmap_ok 
         ((binding <|> bare_expression) <-+> (charp ';'))
         (fun (exp, _) rest -> Ok (exp, rest))
 
+(* parse a function of the form "function name () { statements }" *)
 let funcp = pmap_ok
         ((keyword "function") <-+> identifier <-+> (charp '(') <-+> (charp ')') <-+> (charp '{') <-+> (many stmt) <-+> (charp '}'))
         (fun ((((((_, name), _), _), _), stmts), _) rest -> Ok((func name (block stmts)), rest))
