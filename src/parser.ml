dbfac85c968191c8714408111d77c079aa6caee6
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-26 21:59:21 -0600

    Optimize the code

--- src/parser.ml
+++ src/parser.ml
@@ -16,13 +16,17 @@ let term_oper = (parse_op '*' Times) <|> (parse_op '/' Divide)
 
 let binary_op term op ctor = chainl1 (remove_whitespace term) (pmap_ok (remove_whitespace op) (ok_construct ctor))
 
-(* left-associative parse expressions and terms *)
-let term = binary_op number_p term_oper binary
-let expr_binary = binary_op term expr_oper binary
-
-let rec expression_p s = (expr_list_p <|> non_list_expression_p) s
-
-and non_list_expression_p s = ( expr_binary <|> function_p <|> import_p <|> binding_p ) s
+(* parse expressions separated by semicolons *)
+let rec expression_p s = (pmap_ok
+                (
+                        ( expr_binary <|> function_p <|> import_p <|> binding_p ) 
+                        <-+> (maybe ((charp ';') <-+> expression_p))
+                )
+                (fun (e,m_exp) rest -> (match m_exp with
+                        | Some((_, eml)) -> (match eml with
+                                | ExprList el -> Ok (expr_list (e :: el), rest)
+                                | e2 -> Ok(expr_list (e :: e2 :: []), rest))
+                        | None -> Ok (e, rest)))) s
 
 (* parse a let binding of the form "let x = expression" *)
 and binding_p s = pmap_ok
@@ -39,12 +43,13 @@ and function_p s = pmap_ok
         ((keyword "fun") <-+> identifier <-+> (charp '(') <-+> (charp ')') <-+> (charp '{') <-+> expression_p <-+> (charp '}'))
         (flatmap flatten7 (fun (_,name,_,_,_,expr,_) -> (func name expr))) s
 
-(* parse expressions separated by semicolons *)
-and expr_list_p s = (pmap_ok
-                (non_list_expression_p <-+> (charp ';') <-+> expression_p)
-                (flatmap flatten3 (fun (e,_,eml) -> (match eml with
-                        | ExprList el -> (expr_list (e :: el))
-                        | e2 -> (expr_list (e :: e2 :: [])))))) s
+(* left-associative parse expressions *)
+and expr_binary = binary_op (binary_op number_p term_oper binary) expr_oper binary
+
+
+
+
+                        
 
 (* a program is just an expression followed by the end of input *)
 let program = pmap_ok (remove_whitespace (expression_p <-+> eof))
