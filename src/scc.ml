893883941f75d26f2e069b56896fd4ecd7fe5c72
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-29 16:36:55 -0600

    Add flush of repl

--- src/scc.ml
+++ src/scc.ml
@@ -14,6 +14,11 @@ let prompt_line prompt =
   input_line stdin
     ~fix_win_eol: true
 
+let print_string_flush s = 
+  let open Out_channel in
+  print_string s; 
+  flush stdout
+
 (* REPL *)
 let repl = (fun () -> let exiting = ref false in while (not !exiting) do
   (* prompt for a line *)
@@ -24,8 +29,10 @@ let repl = (fun () -> let exiting = ref false in while (not !exiting) do
   | Some line -> (
     (* Parse the line *)
     match (program line) with
-    | Ok (e, _) -> print_string (expression_to_string e)                 
-    | Error (err, input) -> print_endline ("Error parse failed:" ^ (stringify_parser_error err) ^ " (\"" ^ input ^ "\")");)
+    | Ok (e, _) -> 
+        print_string_flush (expression_to_string e)                 
+    | Error (err, input) -> 
+        print_endline ("Error parse failed:" ^ (stringify_parser_error err) ^ " (\"" ^ input ^ "\")");)
   | None -> exiting := true
 done)
 
