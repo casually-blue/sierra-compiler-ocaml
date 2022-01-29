0374823ab4852dac5755daa00853dc5a6c5c1364
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-29 16:34:33 -0600

    Rewrite to use command lib

--- src/scc.ml
+++ src/scc.ml
@@ -1,19 +1,23 @@
 open Core
-open In_channel
-open Out_channel
 
 open Parserlib.Errors
 
 open Ast
 open Parser
 
+let prompt_line prompt = 
+  let open In_channel in
+  let open Out_channel in 
+
+  output_string stderr prompt;
+  flush stderr;
+  input_line stdin
+    ~fix_win_eol: true
+
 (* REPL *)
-let () = let exiting = ref false in while (not !exiting) do
+let repl = (fun () -> let exiting = ref false in while (not !exiting) do
   (* prompt for a line *)
-  print_string ">> ";
-  flush stdout;
-  let line = input_line stdin
-    ~fix_win_eol: true in
+  let line = (prompt_line ">>> ") in
 
   (* parse the line *)
   match line with
@@ -23,4 +27,21 @@ let () = let exiting = ref false in while (not !exiting) do
     | Ok (e, _) -> print_string (expression_to_string e)                 
     | Error (err, input) -> print_endline ("Error parse failed:" ^ (stringify_parser_error err) ^ " (\"" ^ input ^ "\")");)
   | None -> exiting := true
-done
+done)
+
+let scc = 
+  Command.basic
+    ~summary: "Compile sierra source code"
+    ~readme:(fun () -> "Sierra compiler Version 1.0.0")
+    Command.Let_syntax.(
+      let%map_open
+        _ = flag "-o" (optional string)
+          ~doc: "Output file"
+        and input = anon (maybe_with_default "-" ("INPUT" %: Filename.arg_type))
+      in fun () ->
+        match input with
+          | "-" -> repl (); ()
+          | input -> print_endline ("Input file: " ^ input))
+
+let () = Command.run scc
+
