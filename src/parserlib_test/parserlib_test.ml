0beb8e837d3f0e1eed0417a79c64126ed082034a
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-30 17:04:57 -0600

    Rewrite tests to look better

--- src/parserlib_test/parserlib_test.ml
+++ src/parserlib_test/parserlib_test.ml
@@ -1,26 +1,26 @@
 open Parserlib.Types
 open Parserlib.Errors
+open Parserlib.Combinators
 
-module To_test = struct
-  let get_char = Parserlib.Combinators.get_char
-  let parser_result_eq pa pb = (pa = pb)
+open Alcotest
 
-  let pprint_parser_result ppf = function
+let parser_result_stringify ppf = function
     | Ok (c,rest) -> Fmt.pf ppf "%c,%s" c rest
     | Error (_,rest) -> Fmt.pf ppf "%s" rest
-  let presult_testable = Alcotest.testable pprint_parser_result parser_result_eq
-end
+
+let parser_result_ttb = testable parser_result_stringify (=)
+
   let test_onechar () = 
-    Alcotest.(check To_test.presult_testable) "Same result" (ok 'a' "b") (To_test.get_char "ab")
+    (check parser_result_ttb) "Same result" (ok 'a' "b") (get_char "ab")
   let test_nochar () = 
-    Alcotest.(check To_test.presult_testable) "Error no value" (error (EndOfInputError) "") (To_test.get_char "")
+    (check parser_result_ttb) "Error no value" (error (EndOfInputError) "") (get_char "")
 
 let () =
-  Alcotest.run "Parserlib Tests"
+  run "Parserlib Tests"
     [
       ("get_char tests",
       [
-        Alcotest.test_case "test_single_char" `Quick test_onechar;
-        Alcotest.test_case "test_no_char" `Quick test_nochar
+        test_case "test_single_char" `Quick test_onechar;
+        test_case "test_no_char" `Quick test_nochar
       ])
     ]
