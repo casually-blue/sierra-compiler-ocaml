43b319eaa9d3fef4f9b36f24af42712b2f78aaf7
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-31 11:52:27 -0600

    Add flush to printf

--- src/repl.ml
+++ src/repl.ml
@@ -1,11 +1,10 @@
 let prompt_line prompt = 
   let open Core.In_channel in
-  let open Core.Out_channel in 
+  let open Format in
+  let open Fmt in
 
-  output_string stderr prompt;
-  flush stderr;
+  fprintf stderr "%s%!" prompt;
   input_line stdin
-    ~fix_win_eol: true
 
 (* REPL *)
 let repl () = 
@@ -21,8 +20,7 @@ let repl () =
     match (prompt_line ">>> ") with
     (* parse and pretty print line *)
     | Some line -> 
-      printf "%a\n" (pp_parser_result pp_expr) (program line);
-      flush stdout
+      printf "%a\n%!" (pp_parser_result pp_expr) (program line)
     | None -> exiting := true
   done
 
