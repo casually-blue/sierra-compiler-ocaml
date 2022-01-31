16f0dbbfb0b4b4ea488c5ab67f064a417e73de3f
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-30 18:47:32 -0600

    Rewrite tests

--- src/parserlib_test/parserlib_test.ml
+++ src/parserlib_test/parserlib_test.ml
@@ -10,17 +10,16 @@ let parser_result_stringify ppf = function
 
 let parser_result_ttb = testable parser_result_stringify (=)
 
-  let test_onechar () = 
-    (check parser_result_ttb) "Same result" (ok 'a' "b") (get_char "ab")
-  let test_nochar () = 
-    (check parser_result_ttb) "Error no value" (error (EndOfInputError) "") (get_char "")
+let parser_expect_equal expected actual () = 
+  (check parser_result_ttb) "Expected values to be equal" expected actual
 
 let () =
   run "Parserlib Tests"
     [
       ("get_char tests",
       [
-        test_case "test_single_char" `Quick test_onechar;
-        test_case "test_no_char" `Quick test_nochar
+        test_case "test_single_char" `Quick (parser_expect_equal (ok 'a' "b") (get_char "ab"));
+        test_case "test_no_char" `Quick (parser_expect_equal (error (EndOfInputError) "") (get_char ""));
+        test_case "test_only_one_char" `Quick ((parser_expect_equal (ok 'a' "") (get_char "a"))) 
       ])
     ]
