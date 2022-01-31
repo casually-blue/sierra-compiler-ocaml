f83edd55db27d1c0e8e85a3f27d9254adebe98f0
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-31 11:57:40 -0600

    Rewrite where imports are

--- src/repl.ml
+++ src/repl.ml
@@ -1,26 +1,25 @@
+open Format
+open Fmt
+
+open Parserlib.Types
+
+open Ast
+open Parser
+
 let prompt_line prompt = 
   let open Core.In_channel in
-  let open Format in
-  let open Fmt in
-
   fprintf stderr "%s%!" prompt;
   input_line stdin
 
 (* REPL *)
 let repl () = 
   let exiting = ref false in 
-
-  let open Format in
-  let open Parserlib.Types in
-  let open Ast in
-  let open Parser in
-
   while (not !exiting) do
     (* prompt for a line *)
     match (prompt_line ">>> ") with
     (* parse and pretty print line *)
-    | Some line -> 
-      printf "%a\n%!" (pp_parser_result pp_expr) (program line)
+    | Some line -> printf "%a\n%!" (pp_parser_result pp_expr) (program line)
+    (* at eof, set exit flag *)
     | None -> exiting := true
   done
 
