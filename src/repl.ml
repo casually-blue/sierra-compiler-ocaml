d69345e45c3087feb8b41bab09ea452721f71ffd
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-31 11:02:24 -0600

    Clean up some code

--- src/repl.ml
+++ src/repl.ml
@@ -13,7 +13,7 @@ let print_string_flush s =
   flush stdout
 
 (* REPL *)
-let repl = (fun () -> let exiting = ref false in while (not !exiting) do
+let repl () = let exiting = ref false in while (not !exiting) do
   (* prompt for a line *)
   (* parse the line *)
   match (prompt_line ">>> ") with
@@ -23,8 +23,7 @@ let repl = (fun () -> let exiting = ref false in while (not !exiting) do
     | Ok (e, _) -> 
         print_string_flush (Ast.expression_to_string e)                 
     | Error (err, input) -> 
-        print_endline ("Error parse failed:" ^ (Parserlib.Errors.stringify_parser_error err) ^ " (\"" ^ input ^ "\")");)
+        print_endline ("Error parse failed:" ^ (Parserlib.Errors.stringify_parser_error err) ^ " (\"" ^ input ^ "\")"))
   | None -> exiting := true
-
-done)
+done
 
