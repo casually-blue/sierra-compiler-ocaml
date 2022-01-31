c873ab6a3e2692955e623cec192135cdfa933c50
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-30 19:51:48 -0600

    Add match_char tests

--- src/parserlib_test/parserlib_test.ml
+++ src/parserlib_test/parserlib_test.ml
@@ -23,5 +23,17 @@ let () =
         test_case "test_single_char" `Quick (parser_expect_equal (ok 'a' "b") (get_char "ab"));
         test_case "test_no_char" `Quick (parser_expect_equal (error (EndOfInputError) "") (get_char ""));
         test_case "test_only_one_char" `Quick ((parser_expect_equal (ok 'a' "") (get_char "a"))) 
+      ]);
+      ("match_char tests",
+      [
+        test_case "test_match_a" `Quick 
+          (parser_expect_equal 
+            (ok 'a' "b") 
+            ((match_char (fun c -> c == 'a') (ExpectationError "a")) "ab")
+          );
+        test_case "test_error_a" `Quick
+          (parser_expect_equal
+            (error (ExpectationError "a") "bb")
+            ((match_char (fun c -> c == 'a') (ExpectationError "a")) "bb"))
       ])
     ]
