6a9373a71ede83b4775f53505d4e613964a8ca07
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-31 10:40:05 -0600

    Rewrite main

---
+++ src/repl.ml
@@ -0,0 +1,30 @@
+let prompt_line prompt = 
+  let open Core.In_channel in
+  let open Core.Out_channel in 
+
+  output_string stderr prompt;
+  flush stderr;
+  input_line stdin
+    ~fix_win_eol: true
+
+let print_string_flush s = 
+  let open Core.Out_channel in
+  print_string s; 
+  flush stdout
+
+(* REPL *)
+let repl = (fun () -> let exiting = ref false in while (not !exiting) do
+  (* prompt for a line *)
+  (* parse the line *)
+  match (prompt_line ">>> ") with
+  | Some line -> (
+    (* Parse the line *)
+    match (Parser.program line) with
+    | Ok (e, _) -> 
+        print_string_flush (Ast.expression_to_string e)                 
+    | Error (err, input) -> 
+        print_endline ("Error parse failed:" ^ (Parserlib.Errors.stringify_parser_error err) ^ " (\"" ^ input ^ "\")");)
+  | None -> exiting := true
+
+done)
+
--- src/scc.ml
+++ src/scc.ml
@@ -1,42 +1,13 @@
 open Core
+open Command
 
-open Parserlib.Errors
+let main input () = 
+  match input with
+    | "-" -> Repl.repl (); ()
+    | input -> print_endline ("Input file: " ^ input)
 
-open Ast
-open Parser
-
-let prompt_line prompt = 
-  let open In_channel in
-  let open Out_channel in 
-
-  output_string stderr prompt;
-  flush stderr;
-  input_line stdin
-    ~fix_win_eol: true
-
-let print_string_flush s = 
-  let open Out_channel in
-  print_string s; 
-  flush stdout
-
-(* REPL *)
-let repl = (fun () -> let exiting = ref false in while (not !exiting) do
-  (* prompt for a line *)
-  (* parse the line *)
-  match (prompt_line ">>> ") with
-  | Some line -> (
-    (* Parse the line *)
-    match (program line) with
-    | Ok (e, _) -> 
-        print_string_flush (expression_to_string e)                 
-    | Error (err, input) -> 
-        print_endline ("Error parse failed:" ^ (stringify_parser_error err) ^ " (\"" ^ input ^ "\")");)
-  | None -> exiting := true
-
-done)
-
-let scc = 
-  Command.basic
+let () = 
+  run (basic
     ~summary: "Compile sierra source code"
     ~readme:(fun () -> "Sierra compiler Version 1.0.0")
     Command.Let_syntax.(
@@ -44,10 +15,5 @@ let scc =
         _ = flag "-o" (optional string)
           ~doc: "Output file"
         and input = anon (maybe_with_default "-" ("INPUT" %: Filename.arg_type))
-      in fun () ->
-        match input with
-          | "-" -> repl (); ()
-          | input -> print_endline ("Input file: " ^ input))
-
-let () = Command.run scc
+      in (main input)
 
