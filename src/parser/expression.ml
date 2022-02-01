a847d00cdc9046ab3aafceaf2b177960f8e90a2f
Thomas Terry <thomas.terry@sierra-lang.net>
2022-01-31 22:51:42 -0600

    Rewrite expr parser to use function syntax

--- src/parser/expression.ml
+++ src/parser/expression.ml
@@ -11,35 +11,35 @@ open Number
 
 open Ast
 
-let rec expression_p s = (( string_p <|> expr_binary <|> function_p <|> import_p <|> binding_p <|> fncall_p ) <?> (ExpectationError "Expression")) s
+let rec expression_p = function s -> (( string_p <|> expr_binary <|> function_p <|> import_p <|> binding_p <|> fncall_p ) <?> (ExpectationError "Expression")) s
 
 (* parse expressions separated by semicolons *)
-and expression_list_p s = (pmap_ok
+and expression_list_p = function s -> (pmap_ok
   (sepBy 
     (remove_whitespace (charp ';')) 
     (remove_whitespace expression_p))
   (ok_construct expr_list)) s
 
 (* parse a let binding of the form "let x = expression" *)
-and binding_p s = ((pmap_ok
+and binding_p = function s -> ((pmap_ok
   ((keyword "let") <-+> identifier <-+> (charp '=') <-+> expression_p)
   (flatmap flatten4 (fun (_,name,_,exp) -> (binding name exp)))) <?> (ExpectationError "Let binding")) s
 
 (* parse a function call of the form "name()" *)
-and fncall_p s = ((pmap_ok
+and fncall_p = function s -> ((pmap_ok
   (identifier <-+> (charp '(') <-+> (charp ')'))
   (flatmap flatten3 (fun (name,_,_) -> (fncall name)))) <?> (ExpectationError "Function call")) s
 
 (* parse an import of the form "import name" *)
-and import_p s = ((pmap_ok
+and import_p = function s -> ((pmap_ok
   ((keyword "import") <-+> qualified_id_p)
   (flatmap flatten2 (fun (_, qid) -> (import qid)))) <?> (ExpectationError "Import expression")) s
 
 (* parse a function of the form "fun name () { expression }" *)
-and function_p s = ((pmap_ok
+and function_p = function s -> ((pmap_ok
   ((keyword "fun") <-+> (charp '(') <-+> (charp ')') <-+> (charp '{') <-+> expression_p <-+> (charp '}'))
   (flatmap flatten6 (fun (_,_,_,_,expr,_) -> (func expr)))) <?> (ExpectationError "Function definition")) s
 
-(* left-associative parse expressions *)
-and expr_binary s = binary_op (binary_op number_p term_oper binary) expr_oper binary s
+(* left-associative parse arithmentically expressions *)
+and expr_binary = function s -> binary_op (binary_op number_p term_oper binary) expr_oper binary s
 
