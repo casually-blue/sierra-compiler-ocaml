2c9c8fb57c91a24823df32688f21588fe762d4c9
Thomas Terry <thomas.terry@sierra-lang.net>
2022-02-02 12:13:57 -0600

    Add number tests

--- src/parserlib_test/complex_many_tests.ml
+++ src/parserlib_test/complex_many_tests.ml
@@ -7,8 +7,14 @@ open Parserlib.Types
 let string_list_parser_expect_equal = 
   expect_equal (parser_result_ttb (Format.pp_print_list Format.pp_print_string))
 
+let number_list_parser_ee = 
+  expect_equal (parser_result_ttb (Format.pp_print_list Format.pp_print_int))
+
 let tcase = quick_tcase string_list_parser_expect_equal
+let int_tcase = quick_tcase number_list_parser_ee
+
 let many_ident_sws = (many (remove_whitespace identifier))
+let many_num_sws = (many (remove_whitespace integer))
 
 let complex_many_tests = [
   tcase "no_ident"
@@ -35,4 +41,14 @@ let complex_many_tests = [
   tcase "idents_with_ws_before_and_after"
     (ok ["aaa"; "bbb"] "   ")
     (many_ident_sws "   aaa   bbb   ");
+
+  int_tcase "no_number"
+    (ok [] "")
+    (many_num_sws "");
+  int_tcase "one_number"
+    (ok [22] "")
+    (many_num_sws "22");
+  int_tcase "one_number_ws"
+    (ok [22] "  ")
+    (many_num_sws "  22  ");
 ]
